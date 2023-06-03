d2b5f8e7d12b4c2f0b2e41e240eb786aaf3b9a54
annalie1101 <annalie1101@gmail.com>
2023-06-03 16:54:33 +0530

    train - dataset.py changes - no conflicts

--- utils/datasets.py
+++ utils/datasets.py
@@ -20,6 +20,12 @@ from PIL import Image, ExifTags
 from torch.utils.data import Dataset
 from tqdm import tqdm
 
+import pickle
+from copy import deepcopy
+# from pycocotools import mask as maskUtils
+from torchvision.utils import save_image
+from torchvision.ops import roi_pool, roi_align, ps_roi_pool, ps_roi_align
+
 from utils.general import check_requirements, xyxy2xywh, xywh2xyxy, xywhn2xyxy, xyn2xy, segment2box, segments2boxes, \
     resample_segments, clean_str
 from utils.torch_utils import torch_distributed_zero_first
@@ -1201,7 +1207,7 @@ def pastein(image, labels, sample_labels, sample_images, sample_masks):
 
         box = np.array([xmin, ymin, xmax, ymax], dtype=np.float32)
         if len(labels):
-            ioa = bbox_ioa(box, labels[:, 1:5])  # intersection over area     
+            ioa = bbox_ioa(box, labels[:, 1:5])  # intersection over area
         else:
             ioa = np.zeros(1)
 
